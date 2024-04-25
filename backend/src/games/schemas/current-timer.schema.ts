01a220415a36a38bab8e90e1791e31f21fd382b8
Darosss <daroing12@gmail.com>
2024-04-25 21:04:54 +0200

    refactor(CurrentTimerSchema): rename to proper type names

--- backend/src/games/schemas/current-timer.schema.ts
+++ backend/src/games/schemas/current-timer.schema.ts
@@ -2,7 +2,7 @@ import { Prop, Schema } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
 import { CurrentTimerGame, CurrentTimerGameStage } from 'src/shared';
 
-export type GameDocument = HydratedDocument<CurrentTimer>;
+export type CurrentTimerDocument = HydratedDocument<CurrentTimer>;
 
 @Schema({ _id: false })
 export class CurrentTimer implements CurrentTimerGame {
