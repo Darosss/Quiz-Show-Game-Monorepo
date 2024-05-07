e7370f0955aaa7a5d4dcc44f45cc88c75b627e7d
Darosss <daroing12@gmail.com>
2024-05-07 11:59:36 +0200

    feat(MenuLinks): make admin links visible only for admins

--- frontend/src/components/navigation/menu-links.tsx
+++ frontend/src/components/navigation/menu-links.tsx
@@ -6,20 +6,37 @@ import { appRoutesList } from "./app-routes-list";
 import { usePathname } from "next/navigation";
 import { FC } from "react";
 import { useRoomLobbyContext } from "../game-session/lobby/room-lobby-context";
+import { RolesUser } from "@/shared/enums";
 
 export const MenuLinks: FC = () => {
-  const { isLoggedIn } = useAuthContext();
+  const {
+    apiUser: {
+      api: { data },
+    },
+    isLoggedIn,
+  } = useAuthContext();
   const {
     currentRoomApi: {
       api: { responseData: roomLobbyData },
     },
   } = useRoomLobbyContext();
-
   const pathname = usePathname();
   return (
     <ul className={styles.menuLinksList}>
       {appRoutesList
-        .filter(({ onlyForLoggedIn }) => isLoggedIn === onlyForLoggedIn)
+        .filter(({ onlyForLoggedIn, onlyForAdmin, onlyForSuperAdmin }) => {
+          if (onlyForLoggedIn && !isLoggedIn) return false;
+          if (!onlyForLoggedIn && isLoggedIn) {
+            return false;
+          } else if (onlyForAdmin && !data.roles.includes(RolesUser.Admin))
+            return false;
+          else if (
+            onlyForSuperAdmin &&
+            !data.roles.includes(RolesUser.SuperAdmin)
+          )
+            return false;
+          return true;
+        })
         .map(({ href, name }) => (
           <li
             key={href}
