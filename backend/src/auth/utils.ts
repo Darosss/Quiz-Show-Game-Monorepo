6ac753d9f41930b96cbbcc35c3b787348ad4c533
darosss <darosss12@proton.me>
2025-05-14 11:29:04 +0200

    feat(backend/auth): make HTTP cookies sameSite "none" on production

--- backend/src/auth/utils.ts
+++ backend/src/auth/utils.ts
@@ -6,7 +6,7 @@ export const REFRESH_TOKEN_NAME = 'refresh_token_qs';
 export const setAccessTokenCookieData = (res: Response, newToken: string) => {
   res.cookie(ACCESS_TOKEN_NAME, newToken, {
     httpOnly: true,
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     secure: process.env.NODE_ENV === 'production',
     maxAge: Number(process.env.ACCESS_TOKEN_EXPIRATION_MS) || 60 * 60 * 1000,
   });
@@ -15,7 +15,7 @@ export const setRefreshTokenCookieData = (res: Response, newToken: string) => {
   res.cookie(REFRESH_TOKEN_NAME, newToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge:
       Number(process.env.REFRESH_TOKEN_EXPIRATION_MS) ||
       7 * 24 * 60 * 60 * 1000,
