468012791f3f45ee6198377af021b74dffa6d1ca
darosss <darosss12@proton.me>
2025-05-26 22:32:30 +0200

    chore(fe/home): update homepage title to "Quiz show!"

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import { HomeContent } from "@/components/home";
 export default function Home() {
   return (
     <div>
-      <h1>Home page of quiz show game</h1>
+      <h1>Quiz show!</h1>
       <div>
         <HomeContent />
       </div>
